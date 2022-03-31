8b0b9451e07d927d9ba54e773d25edbb7d2f1f79
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-03-31 11:19:05 +0900

    ページのパスの名前を設定

---
+++ src/pages/myList/all.jsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function MyListAll() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold underline">MyList一覧ページ</h1>
+    </div>
+  );
+}
