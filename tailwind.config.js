f66ab679af4599df8af8d7f30f5a54763a901964
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-04-02 19:45:00 +0900

    Fix typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     extend: {
       colors: {
         "primary-orange": "#DA5019",
-        "primary-variant-orange": "EC9F05",
+        "primary-variant-orange": "#EC9F05",
         "secondary-cyan": "#79DEE3",
       },
     },
