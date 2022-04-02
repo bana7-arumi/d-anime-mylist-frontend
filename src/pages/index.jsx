786ed83a28c98cac026b0a48a0fdcfee18213e19
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-04-02 23:15:57 +0900

    Add トップページのヘッダ部分の作成

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1 className="text-5xl font-bold underline">Topページ</h1>
     </div>
   );
