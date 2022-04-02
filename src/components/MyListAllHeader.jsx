1c8ba77b875176278ceb2069c16a398a412ac287
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-04-02 20:01:00 +0900

    headerのい色を修正

--- src/components/MyListAllHeader.jsx
+++ src/components/MyListAllHeader.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function myListAllHeader() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-orange-600 p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-primary-orange p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <svg
           className="fill-current h-8 w-8 mr-2"
