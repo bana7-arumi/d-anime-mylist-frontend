1e8e0dec2f95067853d71eb54681295acbd01d0c
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-04-03 20:10:18 +0900

    UIが完成

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,9 +2,45 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Page404() {
+  // https://chicodeza.com/freeitems/saikoro-illust.html
+  const status = 404;
+  const message = "ページが見つかりませんでした";
+
+  const saikoroSize = 100;
   return (
     <div>
-      <h1 className="text-5xl font-bold underline">404</h1>
+      <div className="bg-primary-orange">
+        <div className="h-screen w-screen py-24 text-center">
+          <h1 className="text-8xl font-bold text-white mb-4">{status}</h1>
+          <p className="text-2xl text-teal-200">{message}</p>
+          <div className="flex justify-center my-14">
+            {[...Array(Math.max(3, 0))].map((_, index) => (
+              <div className="mx-3 animate-spin" key={index}>
+                {index == 1 ? (
+                  <Image
+                    src="/images/saikoro/saikoro_1.png"
+                    alt="image"
+                    width={saikoroSize}
+                    height={saikoroSize}
+                  />
+                ) : (
+                  <Image
+                    src="/images/saikoro/saikoro_4.png"
+                    alt="image"
+                    width={saikoroSize}
+                    height={saikoroSize}
+                  />
+                )}
+              </div>
+            ))}
+          </div>
+          <div>
+            <button className="px-8 py-2 bg-primary-variant-orange text-white font-semibold hover:bg-amber-400">
+              トップに戻る
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
