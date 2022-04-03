1eaeb74078cd1c2c1e6f4e8ffb8a3125cd31ec58
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-04-03 11:24:44 +0900

    エラーページのファイルを作成

---
+++ src/pages/404.jsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Page404() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold underline">404</h1>
+    </div>
+  );
+}
---
+++ src/pages/500.jsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Page500() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold underline">500</h1>
+    </div>
+  );
+}
