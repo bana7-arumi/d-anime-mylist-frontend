0d9f9373481e7c262ce916180d3515277cf27dc4
Aruminium <b2201680@photon.chitose.ac.jp>
2022-04-04 23:02:51 +0900

    #2 fix function

--- src/pages/myList/[id].jsx
+++ src/pages/myList/[id].jsx
@@ -11,12 +11,14 @@ export default function MylistId() {
   //API GET
   const [mylistList, setMylistList] = useState([]);
   const [mylistInfo, setMylistInfo] = useState({});
+  const [animeInfo, setAnimeInfo] = useState(mylistList[0]);
 
   useEffect(() => {
     (async () => {
       try {
         const res = await axios.get(`/my-list?id=${id}`);
         console.log(res.data);
+        console.log(res.data.mylist);
         setMylistList(res.data.mylist);
         setMylistInfo(res.data);
       } catch (err) {
@@ -24,50 +26,67 @@ export default function MylistId() {
       }
     })();
   }, [id]);
+  
+  console.log(animeInfo);
+
+  const animeList = (
+    <tbody>
+      {mylistList.map((data, index) => (
+        <tr
+          key={data.id}
+          className="an bi d7 cs dk dl"
+        >
+          {/* index番号 */}
+          <td className="ae dm d9 dn do dk">
+            {index + 1}
+          </td>
+          {/* アニメ */}
+          <td className="ah dq">
+            <div className="ah dr b3">
+              {/* アニメタイトル */}
+              <div className="ao ap ds b3 bstu bv">
+                <span dir="auto">{data.title}</span>
+              </div>
+              {/* 話数 */}
+              <div className="ao du ds b3 bstu ae dp">
+                <span dir="auto">{data.stories}</span>
+              </div>
+            </div>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  );
+
   return (
     <div className="ae af ah ai aj ak al am an">
       <div className="ao apqrs at ai aj">
         {/* 選択されているリスト要素 */}
         <div className="ai au ah av">
           {/* 画像 */}
-          <div>
+          <div className="ah aw ax">
+            <div className="ah b3 b4 b5 b6">
 
+            </div>
           </div>
           {/* アニメタイトル */}
-          <div>
-
+          <div className="ah bk bl">
+            <div className="ai">
+              <div className="ah bm b6 bn">
+                <a className="bo bp bq br al bstu bi">
+                  <div className="ae bt bu bv bw bx">
+                    <span dir="auto">{}</span>
+                  </div>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
         {/* アニメリスト */}
         <div className="ah dg dh">
           <div className="di b4">
             <table className="ah dj b3">
-              <tbody>
-                {mylistList.map((data, index) => (
-                  <tr
-                    id={index}
-                    className="e3 an bi d7 cs dk dl"
-                  >
-                    {/* index番号 */}
-                    <td className="ae dm d9 dn do dk">
-                      {index + 1}
-                    </td>
-                    {/* アニメ */}
-                    <td className="ah dq">
-                      <div className="ah dr b3">
-                        {/* アニメタイトル */}
-                        <div className="ao ap ds b3 bstu bv">
-                          <span dir="auto">{data.title}</span>
-                        </div>
-                        {/* 話数 */}
-                        <div className="ao du ds b3 bstu ae dp">
-                          <span dir="auto">全15話</span>
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
+              {animeList}
             </table>
           </div>
         </div>
@@ -75,7 +94,3 @@ export default function MylistId() {
     </div>
   );
 }
-
-// function Click(index){
-//   if(index.style.vi)
-// }
