25597925faaf8f28153e2d7264668f617e7be1f8
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-04-07 23:47:05 +0900

    Fix POSTの本実装に必要なヘッダーパラメータを付与

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,10 +14,18 @@ export default function Header() {
   useEffect(() => {
     console.log("called");
     (async () => {
+      const headers = {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      };
       try {
-        await axios.post("/my-list", {
-          url: setmylisturl,
-        });
+        await axios.post(
+          "/my-list",
+          {
+            url: setmylisturl,
+          },
+          { headers }
+        );
       } catch (err) {
         console.log(err);
       }
@@ -30,7 +38,6 @@ export default function Header() {
         <div className="flex justify-center">
           <h1 className="my-32  text-secondary-cyan text-6xl font-bold">
             What would you like to embed?
-            <p>ボタンクリック：{message}</p>
           </h1>
           <LogoIcon />
         </div>
@@ -40,11 +47,29 @@ export default function Header() {
               className="shadow appearance-none border rounded w-1/2 py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="search"
               type="search"
-              placeholder="マイリストのURLを入力"
+              placeholder="マイリストのURLを入力してください"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             />
-            <button onClick={handleClick}>送信</button>
+            <button
+              className="inline-flex items-center px-3 bg-primary-orange rounded-r-lg"
+              onClick={handleClick}
+            >
+              <svg
+                className="w-6 h-6 text-primary-variant-orange"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                ></path>
+              </svg>
+            </button>
           </div>
         </div>
       </div>
