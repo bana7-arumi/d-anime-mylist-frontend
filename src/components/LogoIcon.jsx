77fd25b5715207892f82eb62a5557058871eb3b0
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-04-09 00:22:22 +0900

    Add width,heightに任意のパラメータを指定可能なロゴアイコンを作成

---
+++ src/components/LogoIcon.jsx
@@ -0,0 +1,237 @@
+export default function LogoIcon(props) {
+  const width = props.width;
+  const height = props.height;
+  return (
+    <svg
+      width={width ? width : 180}
+      height={height ? height : 180}
+      viewBox="0 0 180 180"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_3_65)">
+        <rect
+          width="89.971"
+          height="65.9453"
+          transform="matrix(0.999627 0.0273246 -0.0509087 0.998703 28.3572 69.4167)"
+          fill="#93A5B1"
+          fill-opacity="0.5"
+          shape-rendering="crispEdges"
+        />
+      </g>
+      <g filter="url(#filter1_d_3_65)">
+        <rect
+          width="89.971"
+          height="65.9453"
+          transform="matrix(0.999627 0.0273246 -0.0509087 0.998703 61.3572 42.3222)"
+          fill="#93A5B1"
+          fill-opacity="0.5"
+          shape-rendering="crispEdges"
+        />
+      </g>
+      <g filter="url(#filter2_d_3_65)">
+        <rect
+          width="84.8687"
+          height="72.3938"
+          transform="matrix(0.925176 0.379539 -0.607607 0.794238 72.5793 12)"
+          fill="#93A5B1"
+          fill-opacity="0.5"
+          shape-rendering="crispEdges"
+        />
+      </g>
+      <g filter="url(#filter3_d_3_65)">
+        <rect
+          width="84.8687"
+          height="72.3938"
+          transform="matrix(0.925176 0.379539 -0.607607 0.794238 69.9869 78.2042)"
+          fill="#93A5B1"
+          fill-opacity="0.5"
+          shape-rendering="crispEdges"
+        />
+      </g>
+      <path
+        d="M99.0528 104.746L53.0318 119.367L55.0738 87.0736L99.0528 104.746Z"
+        fill="#DA5019"
+        fill-opacity="0.8"
+      />
+      <rect
+        width="24.9931"
+        height="38.8312"
+        transform="matrix(0.999679 0.0253252 -0.0471897 0.998886 114.833 65.023)"
+        fill="#DA5019"
+        fill-opacity="0.8"
+      />
+      <rect
+        width="34.2465"
+        height="49.6394"
+        transform="matrix(0.633857 -0.77345 0.915497 0.402324 56 58.5582)"
+        fill="#DA5019"
+        fill-opacity="0.8"
+      />
+      <rect
+        width="6.40605"
+        height="18.2791"
+        transform="matrix(0.633857 -0.77345 0.915497 0.402324 74.2266 61.0553)"
+        fill="#93A5B1"
+        fill-opacity="0.5"
+      />
+      <ellipse
+        rx="5.45625"
+        ry="4.45815"
+        transform="matrix(0.990648 0.136443 -0.248771 0.968562 81.3823 47.4223)"
+        fill="#93A5B1"
+        fill-opacity="0.5"
+      />
+      <ellipse
+        rx="5.45625"
+        ry="4.45815"
+        transform="matrix(0.990648 0.136443 -0.248771 0.968562 104.473 56.5637)"
+        fill="#93A5B1"
+        fill-opacity="0.5"
+      />
+      <rect width="180" height="180" rx="43" fill="white" fill-opacity="0.2" />
+      <defs>
+        <filter
+          id="filter0_d_3_65"
+          x="21"
+          y="69.4167"
+          width="101.295"
+          height="76.3182"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_3_65"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_3_65"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter1_d_3_65"
+          x="54"
+          y="42.3222"
+          width="101.295"
+          height="76.3182"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_3_65"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_3_65"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter2_d_3_65"
+          x="24.5924"
+          y="12"
+          width="130.505"
+          height="97.7089"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_3_65"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_3_65"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter3_d_3_65"
+          x="22"
+          y="78.2042"
+          width="130.505"
+          height="97.7089"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_3_65"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_3_65"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
