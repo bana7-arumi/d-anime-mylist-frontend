3f3c175122ee596227831d39c66d14f590ba2b20
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-04-09 00:01:31 +0900

    Fix URLフォームホバー時のマウスカーソルを手のアイコンに修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,46 +2,24 @@ import LogoIcon from "./LogoIcon";
 import { useState, useEffect } from "react";
 
 export default function Header(props) {
-  const [value, setValue] = useState("");
-
   return (
-    <div className="p-32 bg-primary-orange shadow-md">
+    <div className="p-48 bg-primary-orange shadow-md">
       <div>
         <div className="flex justify-center">
-          <h1 className="my-16  text-secondary-cyan text-4xl font-bold">
-            What would you like to embed?
+          <h1 className="my-16 mx-16 text-white text-4xl font-bold">
+            Let's d-anime-embed!
           </h1>
           <LogoIcon />
         </div>
         <div className="relative">
           <div className="flex justify-center p-2 mb-2">
             <input
-              className="shadow appearance-none border rounded-l-lg w-1/2 py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="cursor-pointer shadow appearance-none border rounded-lg w-2/3 py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline  hover:bg-gray-200"
               id="search"
               type="search"
               placeholder="マイリストのURLを入力してください"
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
+              onChange={(e) => props.setMylistUrl(e.target.value)}
             />
-            <button
-              className="shadow appearance-none inline-flex items-center px-4 rounded-r-lg"
-              onClick={() => props.setMylistUrl(value)}
-            >
-              <svg
-                className="w-6 h-6 text-primary-variant-orange"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-            </button>
           </div>
         </div>
       </div>
