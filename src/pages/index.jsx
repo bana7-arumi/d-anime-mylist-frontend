057243ff92310397734cc9b3884ff6bdd6f596b9
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-04-09 12:58:54 +0900

    Fix toggle design

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Iframe from "../components/Iframe";
 import Header from "../components/Header";
 import axios from "../utils/axios";
+import { Switch } from "@headlessui/react";
 
 export default function Home() {
   const [isBorder, setIsBorder] = useState(true);
@@ -9,6 +10,18 @@ export default function Home() {
   const [mylistId, setMylistId] = useState("");
   const [width, setWidth] = useState(500);
   const [height, setHeight] = useState(300);
+  const [enabled, setEnabled] = useState(false);
+
+  const test = (flag) => {
+    if (flag == true) {
+      setEnabled(flag);
+      setIsBorder(flag);
+    } else {
+      setEnabled(flag);
+      setIsBorder(flag);
+    }
+    return <Iframe />;
+  };
 
   useEffect(() => {
     console.log("called");
@@ -60,31 +73,22 @@ export default function Home() {
 
       <div className="container mx-auto px-4 flex justify-center">
         <div className="block">
+          <span className="text-gray-700">枠線を設定する</span>
           <div className="mt-2">
-            <div className="mt-4">
-              <div className="mt-2">
-                <label className="inline-flex items-center">
-                  <input
-                    type="radio"
-                    className="form-radio"
-                    name="borderType"
-                    value="枠線なし"
-                    onChange={() => setIsBorder(false)}
-                  />
-                  <span className="ml-2">枠線なし</span>
-                </label>
-                <label className="inline-flex items-center ml-6">
-                  <input
-                    type="radio"
-                    className="form-radio"
-                    name="borderType"
-                    value="枠線あり"
-                    onChange={() => setIsBorder(true)}
-                  />
-                  <span className="ml-2">枠線あり</span>
-                </label>
-              </div>
-            </div>
+            <Switch
+              checked={enabled}
+              onChange={test}
+              className={`${
+                enabled ? "bg-blue-600" : "bg-gray-200"
+              } relative inline-flex items-center h-6 rounded-full w-11`}
+            >
+              <span className="sr-only">Enable notifications</span>
+              <span
+                className={`${
+                  enabled ? "translate-x-6" : "translate-x-1"
+                } inline-block w-4 h-4 transform bg-white rounded-full`}
+              />
+            </Switch>
             <label className="block mt-4">
               <span className="text-gray-700">横幅を選択する</span>
               <select className="form-select mt-1 block w-full">
