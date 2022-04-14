c45106ab1715453d185cd83d933a92799e889c5e
Aruminium <b2201680@photon.chitose.ac.jp>
2022-04-14 23:02:57 +0900

    fix axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const myAxios = axios.create({
-  baseURL: "http://api.d-anime-mylist.com"
+  baseURL: process.env.NEXT_PUBLIC_BASS_URL ?? "http://localhost:80",
 });
 
 export default myAxios;
